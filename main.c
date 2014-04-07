309bac22bdb61a78bcd03e0b9332e91c0e949c3f
jari <jari@jari-lappari.(none)>
2014-04-07 22:40:04 +0300

    added some interrupt code

--- main.c
+++ main.c
@@ -8,8 +8,24 @@ callback cb_timer1 = NULL;
 callback cb_timer2 = NULL;
 callback cb_timer3 = NULL;
 
+__attribute__ ((__interrupt__(TIMER1_A0_VECTOR)))
+static void timer1_isr()
+{
+	__bic_SR_register_on_exit(LPM4_bits);
+} 
+
+__attribute__((__interrupt__(PORT1_VECTOR)))
+static void port1_isr()
+{
+	P1IFG &= ~BIT3;
+	__bic_SR_register_on_exit(LPM4_bits | GIE);
+} 
+
+
 int main()
 {
+	WDTCTL = WDTPW + WDTHOLD;              // Stop watchdog timer
+
 	while (1) {
 		if (g_event & EV_TIMER1) {
 			cb_timer1();
