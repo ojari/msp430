82c1ae952a5cd5f941efc880a82921888969e4f4
j-a-r-i <jojanen@elisanet.fi>
2014-11-01 16:04:48 +0200

    simple statemachine into timer3 callback function.

--- demo3.c
+++ demo3.c
@@ -17,30 +17,65 @@
 #include "nexa.h"
 #endif
 
+uint8_t stage;
+uint8_t temp1;
+uint8_t temp2;
+#define DS1_PIN 0
+#define DS2_PIN 1
+
 void timer3()
 {
-	uart_str("ping\n");
-	uart_num(g_rtc_hour);
-	uart_str(":");
-	uart_num(g_rtc_min);
-	uart_str(":");
-	uart_num(g_rtc_sec);
-	uart_str("\r\n");
+	//uart_str("ping\n");
 
 	toggle_LED_RED;
 
-	ds1820_init(0);
-	ds1820_read_temp(0);
+	switch (stage) {
+	case 0:
+		uart_num(g_rtc_hour);
+		uart_str(":");
+		uart_num(g_rtc_min);
+		uart_str(":");
+		uart_num(g_rtc_sec);
+		uart_str("  ");
+		uart_num(temp1);
+		uart_str("\r\n");
+		break;
+	case 1:
+		ds1820_measure(DS1_PIN);
+		break;
+	case 2:
+		temp1 = ds1820_read_temp(DS1_PIN);
+		break;
+	case 3:
+		ds1820_measure(DS2_PIN);
+		break;
+	case 4:
+		temp2 = ds1820_read_temp(DS2_PIN);
+		break;
+	case 5:
+		//uart_str("  ");
+		//uart_num(temp1);
+		break;
+	case 6:
+		lcd_clear();
+		lcd_str("Hello");
+		break;
+	case 7:
+		nexa_send(NEXA_CH_1, NEXA_UNIT_1, NEXA_ON);
+		break;
+	}
 
-	lcd_str("Hello");
-
-	nexa_send(NEXA_CH_1, NEXA_UNIT_1, NEXA_ON);
+	stage++;
+	if (stage == 10)
+		stage = 0;
 }
 
 void app_init()
 {
 	START_TIMER3(timer3);	
-
+	stage = 0;
 	nexa_init();
 	lcd_init();
+	ds1820_init(DS1_PIN);
+	ds1820_init(DS2_PIN);
 }
