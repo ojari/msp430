276954ee3c3b4f3109f1440cc89db49baa4ceb75
Jari Ojanen <jojanen@elisanet.fi>
2022-11-12 20:58:04 +0200

    nrf24 driver register definitions

---
+++ nrf24.c
@@ -0,0 +1,57 @@
+#include "hw.h"
+#include "spi.h"
+#include "nrf24.h"
+
+#define PIN_NRF_CE  P2_4
+#define PIN_NRF_CSN P2_3
+
+void nrf24_init()
+{
+    spi_begin();
+
+    digitalWrite(PIN_NRF_CE, LOW);
+    digitalWrite(PIN_NRF_CSN, HIGH);
+
+    spi_transmit(0);  // fix issue in G2553
+
+    nrf24_reg(NRF24_REG_CONFIG, 0x00);
+    nrf24_reg(NRF24_REG_AA, 0x03);
+    nrf24_reg(NRF24_REG_RXADDR, 0x03);
+    nrf24_reg(NRF24_REG_RF_SET, 0x00);
+    //nrf24_reg(NRF24_REG_STATUS, );
+    nrf24_reg(NRF24_REG_DYNPD, 0x03);
+    //nrf24_reg(NRF24_REG_FEATURE, DYNPAYLOAD);
+
+    nrf24_reg(NRF24_CMD_FLUSH_TX, NRF24_CMD_NOP);
+    nrf24_reg(NRF24_CMD_FLUSH_RX, NRF24_CMD_NOP);
+
+    nrf24_reg(NRF24_REG_RF_CH, 10);
+}
+
+void nrf24_begin(uint8_t datarate, uint8_t channel)
+{
+
+}
+
+uint8_t nrf24_available()
+{
+    return 0;
+}
+
+uint8_t nrf24_reg(uint8_t reg, uint8_t value)
+{
+    uint8_t ret;
+
+    digitalWrite(PIN_NRF_CSN, LOW);
+
+    ret = spi_transmit(reg);
+
+    if (reg < NRF24_CMD_FLUSH_TX)
+        ret = spi_transmit(value);
+
+    digitalWrite(PIN_NRF_CSN, HIGH);
+
+    // delay(4us)
+
+    return ret;
+}
---
+++ nrf24.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "hw.h"
+
+#define NRF24_REG_CONFIG    0x00
+#define NRF24_REG_AA        0x01  // auto ack
+#define NRF24_REG_RXADDR    0x02
+#define NRF24_REG_AW        0x03  // address width
+#define NRF24_REG_RETRANS   0x04
+#define NRF24_REG_RF_CH     0x05
+#define NRF24_REG_RF_SET    0x06
+#define NRF24_REG_STATUS    0x07
+#define NRF24_REG_RX_ADDR0  0x0A // RX address pipe0..
+#define NRF24_REG_RX_ADDR1  0x0B
+#define NRF24_REG_RX_ADDR2  0x0C
+#define NRF24_REG_RX_ADDR3  0x0D
+#define NRF24_REG_RX_ADDR4  0x0E
+#define NRF24_REG_RX_ADDR5  0x0F
+#define NRF24_REG_TX_ADDR   0x10
+#define NRF24_REG_RX_PW0    0x11 // RX payload size pipe0..
+#define NRF24_REG_RX_PW1    0x12
+#define NRF24_REG_RX_PW2    0x13
+#define NRF24_REG_RX_PW3    0x14
+#define NRF24_REG_RX_PW4    0x15
+#define NRF24_REG_RX_PW5    0x16
+#define NRF24_REG_FIFO_STAT 0x17
+#define NRF24_REG_DYNPD     0x1C
+#define NRF24_REG_FEATURE   0x1D
+#define NRF24_CMD_FLUSH_TX  0xE1
+#define NRF24_CMD_FLUSH_RX  0xE2
+#define NRF24_CMD_NOP       0xFF
+
+extern void nrf24_init();
+extern void nrf24_begin(uint8_t datarate, uint8_t channel);
+extern void nrf24_set_rx_address(uint8_t *addr);
+extern void nrf24_enable_rx();
+extern uint8_t nrf24_available();
+extern uint8_t nrf24_read(void *buffer, uint8_t length);
+
+
+extern uint8_t nrf24_reg(uint8_t reg, uint8_t value);
